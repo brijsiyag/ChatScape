14f5ef230a3841e8506c65019d8d8cffae238814
brijsiyag <brijsiyag@gmail.com>
2021-09-12 12:13:19 +0530

    Server ready

---
+++ project version 1/public/js/index.js
@@ -0,0 +1,137 @@
+// // const name = prompt("Enter your name");
+const socket = io();
+// const userName = prompt("Enter your name to join");
+
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".container");
+const status = document.querySelector(".wait");
+let socketId;
+
+const myPeer = new Peer();
+const peers = {};
+
+const joinLeaveBtn = document.querySelector(".join-leave-btn");
+const videoGrid = document.querySelector(".vc");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+    myPeer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      video.classList.add("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+    socket.on("user-connected", (userId) => {
+      console.log("User Connected : " + userId);
+      connectToNewUser(userId, stream);
+    });
+  });
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  let video = document.createElement("video");
+  video.classList.add("video");
+
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+
+  call.on("close", () => {
+    video.remove();
+  });
+
+  peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
+
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) {
+    peers[userId].close();
+  }
+  const video = document.querySelector(".video");
+  video.remove();
+
+  if (joinLeaveBtn.innerText == "Join") {
+    joinLeaveBtn.innerText = "Leave";
+  } else if (joinLeaveBtn.innerText == "Leave") {
+    joinLeaveBtn.innerText = "Join";
+  }
+});
+
+function append(message, position) {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+}
+
+function erase(x) {
+  status.innerText = x;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
+});
+
+myPeer.on("open", (id) => {
+  socket.emit("idpasser", id);
+});
+
+joinLeaveBtn.addEventListener("click", (e) => {
+  if (joinLeaveBtn.innerText == "Join") {
+    joinLeaveBtn.innerText = "Leave";
+    socket.emit("joinChat", { socketId: socketId });
+  } else if (joinLeaveBtn.innerText == "Leave") {
+    joinLeaveBtn.innerText = "Join";
+    // if (peers[userId]){
+    //   peers[userId].close();
+    //  }
+    const video = document.querySelector(".video");
+    video.remove();
+    // append(`Stranger left the chat`, 'middle');
+    socket.emit("leave", { socketId: socketId });
+  }
+});
+
+socket.on("disconnection", (name) => {
+  append(`Stranger left the chat`, "middle");
+});
+
+socket.emit("new-user-joined", "Birju");
+
+socket.on("user-joined", (name1) => {
+  append(`${name1} joined the chat`, "right");
+});
+
+socket.on("receive", (message) => {
+  append(`${message.name}: ${message.message}`, "left");
+});
+
+socket.on("leave", (name) => {
+  append(`Stranger left the chat`, "middle");
+});
+
+socket.on("status", (stat) => {
+  status.innerText = stat;
+});
---
+++ project version 1/server.js
@@ -0,0 +1,103 @@
+const bodyParser = require("body-parser");
+const { Socket } = require("dgram");
+const ejs = require("ejs");
+
+const express = require("express");
+const app = express();
+
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, { cors: { origin: "*" } });
+const { v4: uuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("mainChat", { userName: users_name });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const name = req.body.nameInp;
+  // console.log(name);
+  res.redirect("/mainChat");
+});
+
+const users = {};
+const users_name = [];
+let socketId = [];
+let socketPair = {};
+let peerId = {};
+
+io.on("connection", (socket) => {
+  console.log("Socket Array :" + socketId + "  Size :" + socketId.length);
+  socket.on("idpasser", (id) => {
+    // socket.broadcast.emit('user-connected',id);
+    peerId[socket.id] = id;
+    console.log(id);
+  });
+  function pairing() {
+    if (socketId.length > 1) {
+      socketId.splice(socketId.indexOf(socket.id), 1);
+      console.log(
+        "Socket Array after delete of socket.id :" +
+          socketId +
+          "  Size :" +
+          socketId.length
+      );
+      let tempUser = socketId[Math.floor(Math.random() * socketId.length)];
+      socketId.splice(socketId.indexOf(tempUser), 1);
+      socketPair[socket.id] = tempUser;
+      socketPair[tempUser] = socket.id;
+
+      socket.to(tempUser).emit("user-connected", peerId[socket.id]);
+      const stat = "you are now connected to a stranger";
+      socket.to(socketPair[socket.id]).emit("status", stat);
+      socket.emit("status", stat);
+    }
+  }
+  socket.on("joinChat", (data) => {
+    if (socketId.indexOf(socket.id) == -1) {
+      console.log("Socket id getting from leave and join :" + data.socketId);
+      socketId.push(socket.id);
+    }
+    pairing();
+  });
+
+  function left() {
+    const stat = "Disconnected";
+    socket.emit("status", stat);
+    socket.to(socketPair[socket.id]).emit("status", stat);
+    delete socketPair[socket.id];
+    delete socketPair[socketPair[socket.id]];
+  }
+
+  socket.on("leave", (data) => {
+    socket.to(socketPair[socket.id]).emit("disconnection", "Stranger");
+    socket
+      .to(socketPair[socket.id])
+      .emit("user-disconnected", peerId[socket.id]);
+    left();
+  });
+
+  socket.on("send", (message) => {
+    socket
+      .to(socketPair[socket.id])
+      .emit("receive", { message: message, name: "Stranger" });
+  });
+  socket.on("disconnect", (message) => {
+    socket.to(socketPair[socket.id]).emit("disconnection", "Stranger");
+    socket
+      .to(socketPair[socket.id])
+      .emit("user-disconnected", peerId[socket.id]);
+    left();
+  });
+});
+
+http.listen(3000, () => {
+  console.log("Server started at port 3000.");
+});
